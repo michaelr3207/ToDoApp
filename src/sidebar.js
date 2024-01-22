4bf7c5a2d8dd903ecafec93cf3220a8f15278d04
michaelr3207 <michael--robinson321@hotmail.com>
2024-01-22 02:02:44 +0000

    UI changes

--- src/sidebar.js
+++ src/sidebar.js
@@ -4,6 +4,7 @@ import AppLogo from './view-dashboard.png';
 function createSideBarElement(){
     const sideBarElement = createElement('div', 'sidebar', '', '');
     createSideBarLogoAndTitle(sideBarElement);
+    createToDoContainer(sideBarElement);
     return sideBarElement;
 }
 
@@ -18,6 +19,15 @@ function createSideBarLogoAndTitle(sidebarElement){
     sideBarLogoAndTitle.appendChild(sidebarImageElement);
     sideBarLogoAndTitle.appendChild(dashTitleDiv);
     sidebarElement.appendChild(sideBarLogoAndTitle);
+}
+
+function createToDoContainer(sidebarElement){
+    const toDoListContainer = createElement('div', 'toDoListContainer', '', '');
+    const toDoList = createElement('ul', 'toDoList', '', '');
+    const toDOExample = createElement('div', 'toDoDiv', '', 'Walk Dog');
+    toDoList.appendChild(toDOExample);
+    toDoListContainer.appendChild(toDoList);
+    sidebarElement.appendChild(toDoListContainer);
 }
 
 export {createSideBarElement};
